ed65fcec0b2fb9865d524859d46490cb0514d103
_cptSkayt_ <lev.beregovoy.mai@gmail.com>
2025-04-17 09:16:51 +0300

    Убрал все комментарии-напоминания

--- MyNotion/src/components/Task.jsx
+++ MyNotion/src/components/Task.jsx
@@ -13,9 +13,7 @@ const Task = function ({ taskInfo, closeTask, openPopup }) {
             </div>
             <div className="task-button" onClick={() => closeTask(taskInfo)}>{taskInfo.isDone ? "✓" : ""}</div>
             <div className="cross-line"></div>
-        </div>              // ! В данный момент мы работаем над нормальным hover для таска
-        // * Сейчас в проекте 3 ветки: main, task-hover - первый вариант с блоком текста и неполным hover 
-        // * variant2 - ветка для второго варианта, в котором мы сделаем отдельный блок таска и кнопку поверх этого блока с помощью relative и absolute
+        </div>
     )
 }
 
