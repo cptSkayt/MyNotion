a15e762e1e8307076a963bbf12864c22aa0a32de
_cptSkayt_ <lev.beregovoy.mai@gmail.com>
2025-04-17 09:08:23 +0300

    Убрал комментарии-напоминания, закрепил первый вариант

--- MyNotion/src/App.jsx
+++ MyNotion/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'        //* Сменил ветку на первый вариант реализации
+import { useState } from 'react'
 import './General.css'
 import React from 'react';
 import Title from './components/Title';
