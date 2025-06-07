00f6aa747a882cf0928e654bc9f0fa5478462512
_cptSkayt_ <lev.beregovoy.mai@gmail.com>
2025-06-07 03:46:26 +0300

    bug fix

--- MyNotion/src/components/Note.jsx
+++ MyNotion/src/components/Note.jsx
@@ -12,13 +12,13 @@ const Note = ({ status, title, content, addNote, deleteNewNote, deleteNote }) =>
         return (
             <div className="note-block">
                 <div className="note-title-block new-title">
-                    <input ref={title} className="note-textarea" id="note-title-input"></input>
+                    <input ref={title} className="note-textarea" id="note-title-input" placeholder="Введите название"></input>
                 </div>
                 <pre className="note-content new-content">
-                    <textarea ref={content} className="note-textarea" id="note-content-input"></textarea>
+                    <textarea ref={content} className="note-textarea" id="note-content-input" placeholder="Введите текст"></textarea>
                     <div className="new-note-button-block">
                         <div id="new-note-add-button" className="new-note-button" onClick={() => addNote({title: title.current.value, content: content.current.value})}>Добавить</div>
-                        <div id="new-note-delete-button" className="new-note-button" onClick={() => deleteNewNote()}>Удалить</div>
+                        <div id="new-note-delete-button" className="new-note-button" onClick={() => deleteNewNote()}>Отменить</div>
                     </div>
                 </pre>
             </div>
